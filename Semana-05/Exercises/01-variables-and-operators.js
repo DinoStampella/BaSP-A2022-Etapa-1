98737057591c4db1aefd576d7f7859bfa61894de
Dino Stampella <dino.stampella96@gmail.com>
2022-09-17 17:16:40 -0300

    BaSP-W05-2022: variables and operators

---
+++ Semana-05/Exercises/01-variables-and-operators.js
@@ -0,0 +1,32 @@
+console.log('EXCERCISE 1: VARIABLES AND OPERATORS');
+
+//Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+
+console.log('Excercise 1.a:')
+
+var num1 = 1;
+var num2 = 2;
+var suma = num1 + num2;
+
+console.log(suma);
+
+//Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+
+console.log('Excercise 1.b:');
+
+var firstName = 'Dino ';
+var lastname = 'Stampella';
+var fullName = firstName + lastname;
+
+console.log(fullName);
+
+//Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+
+console.log('Excercise 1.c:');
+
+var firstWord = 'Hola';
+var secondWord = 'Chau';
+var wordsLength = firstWord.length + secondWord.length;
+
+console.log(wordsLength)
+
