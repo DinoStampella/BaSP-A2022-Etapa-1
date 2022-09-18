988e9bfc3e31c38bca2c8bf666fa8c38152f6785
Dino Stampella <dino.stampella96@gmail.com>
2022-09-18 11:41:47 -0300

    BaSP-W05-2022: arrays

---
+++ Semana-05/Exercises/03-arrays.js
@@ -0,0 +1,57 @@
+console.log('EXCERCISE 3: ARRAYS');
+
+//Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] mostrar por consola los meses 5 y 11 (utilizar console.log).
+
+console.log('Excercise 3.a:');
+
+var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+console.log(months[4]);
+console.log(months[10]);
+
+//Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).
+
+console.log('Excercise 3.b:');
+
+console.log(months.sort());
+
+//Agregar un elemento al principio y al final del array (utilizar unshift y push).
+
+console.log('Excercise 3.c:');
+
+months.unshift('Start');
+months.push('End');
+
+console.log(months);
+
+
+//Quitar un elemento del principio y del final del array (utilizar shift y pop).
+
+console.log('Excercise 3.d:');
+
+months.shift();
+months.pop();
+
+console.log(months);
+
+//Invertir el orden del array (utilizar reverse).
+
+console.log('Excercise 3.e:');
+
+console.log(months.reverse());
+
+//Unir todos los elementos del array en un único string donde cada mes este separado por un guión - (utilizar join).
+
+console.log('Excercise 3.f:');
+
+console.log(months.join('-'));
+
+//Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar slice).
+
+console.log('Excercise 3.g:');
+
+var newMonths = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+var sliceMonths = newMonths.slice(4, 11);
+
+console.log(sliceMonths);
